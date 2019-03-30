198392031d7de099e505a43ad5573e8af5b6a2a2
Faiz <891366402@qq.com>
2019-03-30 21:08:36 +0800

    新增报告model，部分测试数据不全，明天我会补全测试数据，在拼接页面

--- app/pods/page-result/index/route.js
+++ app/pods/page-result/index/route.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	model() {
+		return this.get('store').query('salesreport',
+			{"scenario-id": "5c7e3157eeefcc1c9ec104ae"}
+		).then(data => data.get('firstObject'));
+	}
 });
