d22a4b5ba7026c4e28961dff6fbc31aff7c5ef58
EthonWang <wangsen1991@icloud.com>
2019-04-11 20:11:57 +0800

    我的任务 页面

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,6 +1,44 @@
 import Route from '@ember/routing/route';
-// import { hash } from 'rsvp';
+import rsvp from 'rsvp';
+import { A } from '@ember/array';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+	cookies: service(),
+	model() {
+		let store = this.get('store'),
+			cookies = this.get('cookies'),
+			useableProposals = A([]),
+			accountId = cookies.read('account_id');
 
+		return store.query('useableProposal', {
+			'account-id': accountId
+		}).then(data => {
+			useableProposals = data;
+			let promiseArray = A([]);
+
+			promiseArray = useableProposals.map(ele => {
+				return store.query('paper', {
+					'proposal-id': ele.get('proposal').get('id'),
+					'account-id': accountId
+				});
+			});
+			return rsvp.Promise.all(promiseArray);
+
+		}).then(data => {
+			return rsvp.hash({
+				papers: data,
+				useableProposals,
+				detailProposal: useableProposals.get('firstObject'),
+				detailPaper: data[0].get('firstObject')
+			});
+		});
+	},
+	actions: {
+		changeDetail(useableProposal, paper) {
+			this.set('model.detailProposal', useableProposal);
+			this.set('model.detailPaper', paper);
+
+		}
+	}
 });
--- app/pods/paperinput/model.js
+++ app/pods/paperinput/model.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	paperId: DS.attr('string'),
 	phase: DS.attr('number'),
+	time: DS.attr('date'),
 	businessinputs: DS.hasMany('businessinput'),
 	managerinputs: DS.hasMany('managerinput'),
 	representativeinputs: DS.hasMany('representativeinput')
