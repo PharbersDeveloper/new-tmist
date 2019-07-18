dee9a4283f21d03e2be9701747d4539b977cff45
AlfredYang1986 <yangyuanpig@gmail.com>
2019-07-18 11:59:56 +0800

    一层封装

--- app/src/components/project/proposal-info/component.js
+++ app/src/components/project/proposal-info/component.js
@@ -1,4 +1,18 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    groupValue: 'index',
+	salesGroupValue: 0,
+	actions: {
+		linkToRoute(routeCode) {
+			let proposalId = this.get('proposalId'),
+				route = routeCode === 'index' ? '' : routeCode;
+
+			this.set('groupValue', routeCode);
+			this.transitionToRoute('/scenario/' + proposalId + '/reference/' + route);
+		},
+		changeSalesValue(value) {
+			this.set('salesGroupValue', value);
+		}
+	}
 });
