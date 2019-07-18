e2a0edbbe15d4aa0a823d0ba448fac98220d3469
AlfredYang1986 <yangyuanpig@gmail.com>
2019-07-18 16:48:03 +0800

    resource model update

--- app/src/model/resource/model.js
+++ app/src/model/resource/model.js
@@ -3,5 +3,13 @@ import DS from "ember-data"
 export default DS.Model.extend( {
 	name: DS.attr( "string" ),
 	gender: DS.attr( "number" ),
-	avatar: DS.belongsTo( "model/image" )
+	avatar: DS.belongsTo( "model/image" ),
+	age: DS.attr("number"),
+    education: DS.attr("string"),
+    professional: DS.attr("string"),
+    advantage: DS.attr("string"),
+    evaluation: DS.attr("string"),
+    experience: DS.attr("string"),
+    totalTime: DS.attr("number"),
+    entryTime: DS.attr("number")
 } )
