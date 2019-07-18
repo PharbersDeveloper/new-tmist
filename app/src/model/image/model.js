6d30704ec97b2e8ef4c4908cfe289f984d8cdce6
fuhanyi <simonzoo@yeah.net>
2019-07-18 14:59:52 +0800

    修正error

--- app/src/model/image/model.js
+++ app/src/model/image/model.js
@@ -1,7 +1,7 @@
-import DS from 'ember-data';
+import DS from "ember-data"
 
-export default DS.Model.extend({
+export default DS.Model.extend( {
 	img: DS.attr( "string" ),
 	tag: DS.attr( "string" ),
 	flag: DS.attr( "number" )
-});
+} )
