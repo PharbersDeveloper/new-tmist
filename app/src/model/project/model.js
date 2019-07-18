a0c9bf1dd5c093f98fddca2c8ca413038bd20cf5
AlfredYang1986 <yangyuanpig@gmail.com>
2019-07-18 22:07:17 +0800

    修改编译bug

--- app/src/model/project/model.js
+++ app/src/model/project/model.js
@@ -5,5 +5,6 @@ export default DS.Model.extend({
     proposal: DS.belongsTo("model/proposal"),
     current: DS.attr("number"),
     pharse: DS.attr("number"),
-    status: DS.attr("number")
+    status: DS.attr("number"),
+    lastUpdate: DS.attr("number")
 });
