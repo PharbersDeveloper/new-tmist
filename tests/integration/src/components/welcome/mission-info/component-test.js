e4903eac7bdb92ac838b1486f0dbf93951ceecaa
fuhanyi <simonzoo@yeah.net>
2019-07-18 10:48:51 +0800

    修正 scss error

--- tests/integration/src/components/welcome/mission-info/component-test.js
+++ tests/integration/src/components/welcome/mission-info/component-test.js
@@ -1,26 +1,26 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from "qunit"
+import { setupRenderingTest } from "ember-qunit"
+import { render } from "@ember/test-helpers"
+import hbs from "htmlbars-inline-precompile"
 
-module('Integration | Component | welcome/misson-info', function(hooks) {
-  setupRenderingTest(hooks);
+module( "Integration | Component | welcome/misson-info", function( hooks ) {
+	setupRenderingTest( hooks )
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+	test( "it renders", async function( assert ) {
+		// Set any properties with this.set('myProperty', 'value');
+		// Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{welcome/misson-info}}`);
+		await render( hbs`{{welcome/misson-info}}` )
 
-    assert.equal(this.element.textContent.trim(), '');
+		assert.equal( this.element.textContent.trim(), "" )
 
-    // Template block usage:
-    await render(hbs`
+		// Template block usage:
+		await render( hbs`
       {{#welcome/misson-info}}
         template block text
       {{/welcome/misson-info}}
-    `);
+    ` )
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
-});
+		assert.equal( this.element.textContent.trim(), "template block text" )
+	} )
+} )
