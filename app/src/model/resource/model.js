8377cef5567e8f12db2e10a61d36f77fcae941b8
fuhanyi <simonzoo@yeah.net>
2019-07-18 18:43:34 +0800

    Add member info

--- app/src/model/resource/model.js
+++ app/src/model/resource/model.js
@@ -4,12 +4,12 @@ export default DS.Model.extend( {
 	name: DS.attr( "string" ),
 	gender: DS.attr( "number" ),
 	avatar: DS.belongsTo( "model/image" ),
-	age: DS.attr("number"),
-    education: DS.attr("string"),
-    professional: DS.attr("string"),
-    advantage: DS.attr("string"),
-    evaluation: DS.attr("string"),
-    experience: DS.attr("string"),
-    totalTime: DS.attr("number"),
-    entryTime: DS.attr("number")
+	age: DS.attr( "number" ),
+	education: DS.attr( "string" ),
+	professional: DS.attr( "string" ),
+	advantage: DS.attr( "string" ),
+	evaluation: DS.attr( "string" ),
+	experience: DS.attr( "string" ),
+	totalTime: DS.attr( "number" ),
+	entryTime: DS.attr( "number" )
 } )
