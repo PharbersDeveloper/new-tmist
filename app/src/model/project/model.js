cbbdb65467d7925f6436efdeff6d5e5438e4c386
fuhanyi <simonzoo@yeah.net>
2019-07-19 10:09:53 +0800

    修正error

--- app/src/model/project/model.js
+++ app/src/model/project/model.js
@@ -1,10 +1,10 @@
-import DS from 'ember-data';
+import DS from "ember-data"
 
-export default DS.Model.extend({
-    accountId: DS.attr("string"),
-    proposal: DS.belongsTo("model/proposal"),
-    current: DS.attr("number"),
-    pharse: DS.attr("number"),
-    status: DS.attr("number"),
-    lastUpdate: DS.attr("number")
-});
+export default DS.Model.extend( {
+	accountId: DS.attr( "string" ),
+	proposal: DS.belongsTo( "model/proposal" ),
+	current: DS.attr( "number" ),
+	pharse: DS.attr( "number" ),
+	status: DS.attr( "number" ),
+	lastUpdate: DS.attr( "number" )
+} )
