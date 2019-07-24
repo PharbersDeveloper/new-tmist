3a73180549c192fb98dabb87516b387360681a5d
fuhanyi <simonzoo@yeah.net>
2019-07-24 11:50:50 +0800

    重置功能

--- app/src/components/project/management-input/manager-time-management/component.js
+++ app/src/components/project/management-input/manager-time-management/component.js
@@ -153,49 +153,27 @@ export default Component.extend( {
 		]
 	} ),
 	actions: {
-		changeState( context, key ) {
-			let isOverKpi = this.get( "isOverKpi" ),
-				state = Number( context.get( key ) )
-
-			if ( !isOverKpi || state !== 0 ) {
-				context.toggleProperty( key )
-			} else {
-				this.set( "warning", {
-					open: true,
-					title: "经理行动点数超额",
-					detail: "经理无剩余行动点数可供分配。"
-				} )
-			}
-		},
 		reInputTime() {
-			let managerInput = this.get( "model.managerInput" ),
-				representativeInputs = this.get( "model.representativeInputs" )
+			let answers = this.get( "answers" ),
+				managerAnswer = this.get( "managerAnswer" )
 
-			managerInput.setProperties( {
-				strategyAnalysisTime: "",
-				adminWorkTime: "",
-				clientManagementTime: "",
-				kpiAnalysisTime: "",
-				teamMeetingTime: ""
-			} )
-			representativeInputs.forEach( ele => {
+			answers.forEach( ele => {
 				ele.setProperties( {
-					abilityCoach: "",
-					assistAccessTime: ""
+					strategyAnalysisTime: 0,
+					adminWorkTime: 0,
+					clientManagementTime: 0,
+					kpiAnalysisTime: 0,
+					teamMeetingTime: 0,
+					abilityCoach: 0,
+					assistAccessTime: 0
 				} )
 			} )
-		},
-		reInputPoint() {
-			let representativeInputs = this.get( "model.representativeInputs" )
-
-			representativeInputs.forEach( ele => {
-				ele.setProperties( {
-					productKnowledgeTraining: 0,
-					salesAbilityTraining: 0,
-					regionTraining: 0,
-					performanceTraining: 0,
-					vocationalDevelopment: 0
-				} )
+			managerAnswer.setProperties( {
+				strategAnalysisTime: 0,
+				clientManagementTime: 0,
+				adminWorkTime: 0,
+				kpiAnalysisTime: 0,
+				teamMeetingTime: 0
 			} )
 		}
 	}
