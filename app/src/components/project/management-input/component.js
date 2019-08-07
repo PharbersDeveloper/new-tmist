b73ddb057d0c3d7f706a1e6cbfc33092d76ec956
fuhanyi <simonzoo@yeah.net>
2019-08-07 15:50:01 +0800

    样式修改

--- app/src/components/project/management-input/component.js
+++ app/src/components/project/management-input/component.js
@@ -1,6 +1,15 @@
 import Component from "@ember/component"
+import { computed } from "@ember/object"
 
 export default Component.extend( {
 	positionalParams: ["project", "period", "resources", "answers"],
-	groupValue: 0
+	groupValue: 0,
+	currentResource: 0,
+	quizs: computed( "resources", "answers", function () {
+		return this.resources.map( item => {
+			const one = this.answers.find( x => x.get( "resource.id" ) === item.get( "id" ) )
+
+			return { resource: item, answer: one }
+		} )
+	} )
 } )
