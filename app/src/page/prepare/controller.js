657a04b3a6f7fe996eab7bf44b0a5179f8ac0f71
Chenghao Sun <601330984@qq.com>
2019-08-10 18:50:20 +0800

    临时

--- app/src/page/prepare/controller.js
+++ app/src/page/prepare/controller.js
@@ -1,4 +1,9 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    actions: {
+        toHistory() {
+            this.transitionToRoute("page.history")
+        }
+    }
 });
