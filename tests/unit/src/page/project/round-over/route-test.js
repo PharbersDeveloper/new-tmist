243b462833f2197728b0e4b89a371b0de1fe6fa1
fuhanyi <simonzoo@yeah.net>
2019-08-15 20:17:27 +0800

    恭喜你页面

---
+++ tests/unit/src/page/project/round-over/route-test.js
@@ -0,0 +1,11 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Route | page/project/round-over', function(hooks) {
+  setupTest(hooks);
+
+  test('it exists', function(assert) {
+    let route = this.owner.lookup('route:page/project/round-over');
+    assert.ok(route);
+  });
+});
