504d508e44f0738ba0d842fcd53ec6913e83b075
AlfredYang1986 <yangyuanpig@gmail.com>
2019-08-15 09:59:58 +0800

    修改一点东西

--- app/src/model/preset/model.js
+++ app/src/model/preset/model.js
@@ -3,7 +3,21 @@ import DS from "ember-data"
 export default DS.Model.extend( {
 	hospital: DS.belongsTo( "model/hospital" ),
 	product: DS.belongsTo( "model/product" ),
+	resource: DS.belongsTo( "model/resource" ),
 	salesQuota: DS.attr( "number" ),
 	potential: DS.attr( "number" ),
-	achievements: DS.attr( "number" )
+	achievements: DS.attr( "number" ),
+	category: DS.attr("number"),
+	sales: DS.attr("number"),
+	share: DS.attr("number"),
+	territoryManagementAbility: DS.attr("number"),
+	salesSkills: DS.attr("number"),
+	productKnowledge: DS.attr("number"),
+	behaviorEfficiency: DS.attr("number"),
+	workMotivation: DS.attr("number"),
+	targetDoctorNum: DS.attr("number"),
+	targetDoctorCoverage: DS.attr("number"),
+	highTarget: DS.attr("number"),
+	middleTarget: DS.attr("number"),
+	lowTarget: DS.attr("number")
 } )
