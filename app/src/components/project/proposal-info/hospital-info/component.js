0b5cd810859ee3ca89303ba7eac64669e1b477e9
fuhanyi <simonzoo@yeah.net>
2019-08-16 19:34:46 +0800

    医院

--- app/src/components/project/proposal-info/hospital-info/component.js
+++ app/src/components/project/proposal-info/hospital-info/component.js
@@ -1,6 +1,6 @@
 import Component from "@ember/component"
 
 export default Component.extend( {
-	positionalParams: ["hospitals", "reports"],
-	currentHospital: 0,
+	positionalParams: ["hospitals", "reports", "proposal"],
+	currentHospital: 0
 } )
