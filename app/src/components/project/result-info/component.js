344cb2086043f909343e04821e5b4ec14a0b55cb
AlfredYang1986 <yangyuanpig@gmail.com>
2019-08-18 04:05:36 +0800

    修改

--- app/src/components/project/result-info/component.js
+++ app/src/components/project/result-info/component.js
@@ -1,111 +1,111 @@
 import Component from "@ember/component"
-// import { computed } from "@ember/object"
+import { computed } from "@ember/object"
 // import ENV from 'new-tmist/config/environment';
 import { later } from "@ember/runloop"
 
 export default Component.extend( {
-	positionalParams: ["project", "results", "evaluations"],
+	positionalParams: ["project", "results", "evaluations", "reports"],
 	// overallInfo: computed(results function () {
 
 	// } ),
 	overallInfo: null,
 	init() {
-		this._super( ...arguments )
-		new Promise( function ( resolve ) {
-			later( function () {
-				let tmResultProductCircle = {
-						id: "circleproductcontainer1",
-						height: 319,
-						panels: [
-							{
-								name: "tmResultProducts",
-								id: "tmResultProducts",
-								color: ["#73ABFF", "#FFC400", "#57D9A3"],
-								tooltip: {
-									show: true,
-									trigger: "item"
-								},
-								legend: {
-									show: false
-								},
-								series: [{
-									name: "",
-									type: "pie",
-									radius: ["70", "100"],
-									avoidLabelOverlap: false,
-									hoverOffset: 3,
-									labelLine: {
-										normal: {
-											show: true
-										}
-									},
-									label: {
-										color: "#7A869A",
-										formatter: "{b}  {d}%"
-									}
+		// this._super( ...arguments )
+		// new Promise( function ( resolve ) {
+		// 	later( function () {
+		// 		let tmResultProductCircle = {
+		// 				id: "circleproductcontainer1",
+		// 				height: 319,
+		// 				panels: [
+		// 					{
+		// 						name: "tmResultProducts",
+		// 						id: "tmResultProducts",
+		// 						color: ["#73ABFF", "#FFC400", "#57D9A3"],
+		// 						tooltip: {
+		// 							show: true,
+		// 							trigger: "item"
+		// 						},
+		// 						legend: {
+		// 							show: false
+		// 						},
+		// 						series: [{
+		// 							name: "",
+		// 							type: "pie",
+		// 							radius: ["70", "100"],
+		// 							avoidLabelOverlap: false,
+		// 							hoverOffset: 3,
+		// 							labelLine: {
+		// 								normal: {
+		// 									show: true
+		// 								}
+		// 							},
+		// 							label: {
+		// 								color: "#7A869A",
+		// 								formatter: "{b}  {d}%"
+		// 							}
 
-								}]
-							}
-						]
-					},
-					tmResultProductCircleCondition = [{
-						data: {
-							"_source": [
-								"product",
-								"sales",
-								"date",
-								"salesRate"
-							],
-							"query": {
-								"bool": {
-									"must": [
-										{
-											"match": {
-												"date": "2018Q1"
-											}
-										},
-										{
-											"match": {
-												"rep": "all"
-											}
-										},
-										{
-											"match": {
-												"region": "all"
-											}
-										},
-										{
-											"match": {
-												"hosp_level": "all"
-											}
-										},
-										{
-											"match": {
-												"hosp_name": "all"
-											}
-										}
-									],
-									"must_not": [
-										{
-											"match": {
-												"product": "all"
-											}
-										}
-									]
-								}
-							}
-						}
-					}]
+		// 						}]
+		// 					}
+		// 				]
+		// 			},
+		// 			tmResultProductCircleCondition = [{
+		// 				data: {
+		// 					"_source": [
+		// 						"product",
+		// 						"sales",
+		// 						"date",
+		// 						"salesRate"
+		// 					],
+		// 					"query": {
+		// 						"bool": {
+		// 							"must": [
+		// 								{
+		// 									"match": {
+		// 										"date": "2018Q1"
+		// 									}
+		// 								},
+		// 								{
+		// 									"match": {
+		// 										"rep": "all"
+		// 									}
+		// 								},
+		// 								{
+		// 									"match": {
+		// 										"region": "all"
+		// 									}
+		// 								},
+		// 								{
+		// 									"match": {
+		// 										"hosp_level": "all"
+		// 									}
+		// 								},
+		// 								{
+		// 									"match": {
+		// 										"hosp_name": "all"
+		// 									}
+		// 								}
+		// 							],
+		// 							"must_not": [
+		// 								{
+		// 									"match": {
+		// 										"product": "all"
+		// 									}
+		// 								}
+		// 							]
+		// 						}
+		// 					}
+		// 				}
+		// 			}]
 
-				resolve( {
-					tmResultProductCircle, tmResultProductCircleCondition
-				} )
+		// 		resolve( {
+		// 			tmResultProductCircle, tmResultProductCircleCondition
+		// 		} )
 
-			}, 400 )
-		} ).then( data => {
-			this.set( "tmResultProductCircleCondition", data.tmResultProductCircleCondition )
-			this.set( "tmResultProductCircle", data.tmResultProductCircle )
-		} )
+		// 	}, 400 )
+		// } ).then( data => {
+		// 	this.set( "tmResultProductCircleCondition", data.tmResultProductCircleCondition )
+		// 	this.set( "tmResultProductCircle", data.tmResultProductCircle )
+		// } )
 	},
 	didReceiveAttrs() {
 		this._super( ...arguments )
@@ -118,25 +118,28 @@ export default Component.extend( {
 			awardImg: ""
 		}
 
-		this.results.map( ele => {
-			this.evaluations.map( elem => {
-				if ( ele.category === "Overall" && elem.category === "Overall" ) {
-					if ( ele.get( "awardLevel.rank" ) === elem.level ) {
-						tmpOverall.abilityLevel = ele.get( "awardLevel.rank" )
-						tmpOverall.abilityDes = elem.abilityDescription
-						tmpOverall.abilityImg = ele.abilityLevel.get( "rankImg" )
-					}
-					if ( ele.get( "awardLevel.rank" ) === elem.level ) {
-						tmpOverall.awardLevel = ele.get( "awardLevel.rank" )
-						tmpOverall.awardDes = elem.awardDescription
-						tmpOverall.awardImg = ele.abilityLevel.get( "awardImg" )
-					}
-				}
-			} )
-		} )
+		// this.results.map( ele => {
+		// 	this.evaluations.map( elem => {
+		// 		if ( ele.category === "Overall" && elem.category === "Overall" ) {
+		// 			if ( ele.get( "awardLevel.rank" ) === elem.level ) {
+		// 				tmpOverall.abilityLevel = ele.get( "awardLevel.rank" )
+		// 				tmpOverall.abilityDes = elem.abilityDescription
+		// 				tmpOverall.abilityImg = ele.abilityLevel.get( "rankImg" )
+		// 			}
+		// 			if ( ele.get( "awardLevel.rank" ) === elem.level ) {
+		// 				tmpOverall.awardLevel = ele.get( "awardLevel.rank" )
+		// 				tmpOverall.awardDes = elem.awardDescription
+		// 				tmpOverall.awardImg = ele.abilityLevel.get( "awardImg" )
+		// 			}
+		// 		}
+		// 	} )
+		// } )
 
 		this.set( "overallInfo", tmpOverall )
 	},
+	salesReport: computed("project", function() {
+		return this.project.get("finals").lastObject
+	} ),
 	actions: {
 		toReport() {
 			this.transitionToReport()
