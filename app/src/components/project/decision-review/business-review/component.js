2e73cd8657b06251b4772ac29c5d2820b59ce609
Faiz <891366402@qq.com>
2019-08-18 21:32:15 +0800

    决策回顾-逻辑数据

--- app/src/components/project/decision-review/business-review/component.js
+++ app/src/components/project/decision-review/business-review/component.js
@@ -1,42 +1,82 @@
 import Component from "@ember/component"
 import { computed } from "@ember/object"
+import { A } from "@ember/array"
 
 export default Component.extend( {
 	positionalParams: ["proposol", "project", "hospitals", "resources", "products", "answers"],
 	classNames: ["business-review-wrapper"],
-	curProd: null,
-	curRes: null,
-	periodRange: computed("periodLength", function() {
-		return Array(this.periodLength).fill().map((e,i)=>i)
+	curProd: computed( function () {
+		return { name: "全部" }
 	} ),
-	periodLength: computed("project", function() {
+	curRes: computed( function () {
+		return { name: "全部" }
+	} ),
+	productList: computed( "products", function () {
+		let arr = []
+
+		this.products.forEach( x => {
+			arr.push( x )
+		} )
+		arr.push( { name: "全部" } )
+
+		return A( arr )
+	} ),
+	resourcesList: computed( "resources", function () {
+		let arr = []
+
+		this.resources.forEach( x => {
+			arr.push( x )
+		} )
+		arr.push( { name: "全部" } )
+
+		return A( arr )
+	} ),
+	periodsName: computed( "project", function () {
+		let name = []
+
+		this.project.periods.forEach( x => {
+			name.push( x.name )
+		} )
+		return A( name )
+	} ),
+	periodRange: computed( "periodLength", function () {
+		return Array( this.periodLength ).fill().map( ( e, i ) => i )
+	} ),
+	periodLength: computed( "project", function () {
 		return this.project.periods.length
 	} ),
-	curPeriodIndex: computed("project", function() {
+	curPeriodIndex: computed( "project", function () {
 		return this.project.periods.length - 1
 	} ),
-	curPeriod: computed("curPeriodIndex", function() { 
-		return this.project.periods.objectAt(this.curPeriodIndex)
+	curPeriod: computed( "curPeriodIndex", function () {
+		return this.project.periods.objectAt( this.curPeriodIndex )
 	} ),
-	curAnswers: computed("curPeriod", function() {
+	curAnswers: computed( "curPeriod", function () {
 		// const condi01 = "(proposalId,:eq,`" + x.id + "`)"
 		// const condi02 = "(phase,:eq,-1)"
 		// const condi = "(:and," + condi01 + "," + condi02 + ")"
 		// return this.store.query("model/answer", { filter: })
-		if (this.curPeriodIndex === 0) {
+		if ( this.curPeriodIndex === 0 ) {
 			return this.answers
 		} else {
 			return []
 		}
 	} ),
-	filterAnswers: computed("curAnswers", "curProd", "curRes", function() {
-		let result = this.curAnswers.filter(x => x.category === "Business")
-		if (this.curProd) {
-			result = result.filter(x => x.get("product.id") === this.curProd.get("id"))
+	filterAnswers: computed( "curAnswers", "curProd", "curRes", function () {
+		let result = this.curAnswers.filter( x => x.category === "Business" )
+
+		if ( this.curProd ) {
+			if ( this.curProd.name !== "全部" ) {
+				// result = result
+				result = result.filter( x => x.get( "product.id" ) === this.curProd.get( "id" ) )
+			}
 		}
 
-		if (this.curRes) {
-			result = result.filter(x => x.resource.get("id") === this.curRes.get("id"))
+		if ( this.curRes ) {
+			if ( this.curRes.name !== "全部" ) {
+				// result = result
+				result = result.filter( x => x.resource.get( "id" ) === this.curRes.get( "id" ) )
+			}
 		}
 		return result
 	} )
