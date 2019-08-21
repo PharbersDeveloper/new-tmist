dcf0f97cc048c57460a0045811bac611d6a715d0
Chenghao Sun <601330984@qq.com>
2019-08-21 16:42:00 +0800

    周期问题

--- app/src/page/project/period/controller.js
+++ app/src/page/project/period/controller.js
@@ -122,7 +122,7 @@ export default Controller.extend( {
 				data: {
 					callr: false,
 					type: type,
-					phase: String( Number( phase ) + 1 ),
+					phase: String( phase ),
 					proposalId: proposalId,
 					projectId: projectId,
 					periodId: periodId
