0badada046fb15b28f4e82c9a4088d79772f2e97
fuhanyi <simonzoo@yeah.net>
2019-09-05 19:46:26 +0800

    样式修改

--- tests/integration/helpers/format-date-standard-test.js
+++ tests/integration/helpers/format-date-standard-test.js
@@ -1,17 +1,17 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from "qunit"
+import { setupRenderingTest } from "ember-qunit"
+import { render } from "@ember/test-helpers"
+import hbs from "htmlbars-inline-precompile"
 
-module('Integration | Helper | format-date-standard', function(hooks) {
-  setupRenderingTest(hooks);
+module( "Integration | Helper | format-date-standard", function( hooks ) {
+	setupRenderingTest( hooks )
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+	// Replace this with your real tests.
+	test( "it renders", async function( assert ) {
+		this.set( "inputValue", "1234" )
 
-    await render(hbs`{{format-date-standard inputValue}}`);
+		await render( hbs`{{format-date-standard inputValue}}` )
 
-    assert.equal(this.element.textContent.trim(), '1234');
-  });
-});
+		assert.equal( this.element.textContent.trim(), "1234" )
+	} )
+} )
