b07733dc60fdc8859169142324bb8363e0d1e722
frank <wangsen9108@gmail.com>
2019-09-12 18:30:37 +0800

    添加参考信息/结果报告的表格（存在性能问题）

--- config/environment.js
+++ config/environment.js
@@ -29,8 +29,8 @@ module.exports = function( environment ) {
 
 		OAuth: {
 			Version: "v0",
-			// ClientId: "5d5a33c145b5387e411a08f0",
-			ClientId: "5cbd9f94f4ce4352ecb082a0",
+			ClientId: "5d5a33c145b5387e411a08f0",
+			// ClientId: "5cbd9f94f4ce4352ecb082a0",
 			// ClientId: "5d68d46bb69b1d3f22693d26", //UCB专版
 			ClientSecret: "5c90db71eeefcc082c0823b2",
 			Status: "self",
@@ -59,7 +59,7 @@ module.exports = function( environment ) {
 		ENV.APP.LOG_TRANSITIONS_INTERNAL = true
 		ENV.APP.LOG_VIEW_LOOKUPS = true
 
-		ENV.OAuth.RedirectUri = "http://tm.pharbers.com"
+		ENV.OAuth.RedirectUri = "http://tm.pharbers.com:8081"
 		// ENV.OAuth.RedirectUri = "http://ucb.pharbers.com" //UCB专版
 		ENV.OAuth.Host = "http://oauth.pharbers.com"
 		ENV.QueryAddress.host = "http://59.110.31.50"
