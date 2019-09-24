54f16d873affe07f7d9a7f115ffb6f17164ec4df
fuhanyi <simonzoo@yeah.net>
2019-09-24 17:46:53 +0800

    样式文案修改，排序修改

--- app/src/components/project/business-input/resource-config/component.js
+++ app/src/components/project/business-input/resource-config/component.js
@@ -31,7 +31,7 @@ export default Component.extend( {
 			all += this.transNumber( a.get( "visitTime" ) )
 		} )
 
-		return 100 - all
+		return all
 	} ),
 	transNumber( input ) {
 		let number = Number( input )
